3ff512919e1b70c3e1ec84ea5357c882fa51d7ae
govindchoudhary0981@gmail.com <govindchoudhary0981@gmail.com>
2025-04-21 09:56:05 +0530

    added more extra Problems

---
+++ Extra Problems/Array_problems/08_problem8.cpp
@@ -0,0 +1,34 @@
+
+/*
+Problem: Trapping Rainwater
+
+You are given an array `height[]` of size `n` representing the height of walls
+where the width of each wall is 1 unit. After it rains, some water is trapped between the walls.
+
+Your task is to calculate how much water can be trapped between the walls.
+
+
+Input:
+- `height`: An array of `n` non-negative integers.
+
+Output:
+- An integer representing the total units of trapped water.
+
+
+Example:
+
+Input:  height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+
+Water is trapped between the bars — total 6 units.
+
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
