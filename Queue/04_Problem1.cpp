d630c05604dddef7454ec402acbd18d2b5ae66f2
govindchoudhary0981@gmail.com <govindchoudhary0981@gmail.com>
2025-05-21 14:55:05 +0530

    Added new queue notes

--- Queue/04_Problem1.cpp
+++ Queue/04_Problem1.cpp
@@ -33,39 +33,62 @@ false
 #include<stack>
 using namespace std;
 
+// Implementing a Queue using two Stacks (s1 and s2)
 class Queue {
-    stack<int>s1;
-    stack<int>s2;
+    stack<int> s1; // Main stack where front of queue is always on top
+    stack<int> s2; // Helper stack used during push operation
 
 public:
 
-    void push(int val){
-        // s1 -> s2
-        while (!s1.empty()){
+    // Function to insert an element into the queue
+    void push(int val) {
+        // Step 1: Move all elements from s1 to s2
+        while (!s1.empty()) {
             s2.push(s1.top());
             s1.pop();
         }
 
+        // Step 2: Push the new element onto s1
         s1.push(val);
 
-        // s2 -> s1
-        while (!s2.empty()){
+        // Step 3: Move everything back from s2 to s1
+        while (!s2.empty()) {
             s1.push(s2.top());
             s2.pop();
         }
+
+        // Now, the front of the queue is at the top of s1
     }
 
-    void pop(){
-        s1.pop();
+    // Function to remove the front element of the queue
+    void pop() {
+        // Since front is on top of s1, just pop
+        if (!s1.empty())
+            s1.pop();
     }
 
-    int front(){
-        return s1.top();
+    // Function to get the front element of the queue
+    int front() {
+        // Return the top element of s1 which is front of queue
+        if (!s1.empty()){
+            return s1.top();
+        }
+        return -1; // Return -1 if queue is empty (edge case)
     }
-    
 };
 
-int main(){
+int main() {
+    Queue q;
+
+    // Insert elements
+    q.push(1);
+    q.push(2);
+    q.push(3); // Queue is now: 1 (front), 2, 3
+
+    q.pop();   // Removes 1
+
+    // Should print 2 (new front)
+    cout << "Front : " << q.front() << endl;
 
     return 0;
 }
